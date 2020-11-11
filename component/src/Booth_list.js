add73943b4e2bcf10e01a4ed2edb39fe3fb3e08c
youngho-C <cyh6099@gmail.com>
2020-11-11 21:37:53 +0900

    booth list

--- component/src/Booth_list.js
+++ component/src/Booth_list.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './booth_list.css'
-import sam from './sam.png'
+import sam from './images/sam.png'
+
+const obj = [
+    {
+        name : '예술대학 분장',
+        place : "B-1"
+    },
+    {
+        name : '소융 분장',
+        place : "B-1"
+    },
+    {
+        name : '예술대학 분장',
+        place : "B-1"
+    },
+]
 
 function Booth_list(){
     return(
         <div id="box">
             <div id="tap_bar"></div>
             <p id="list_title">이 시간 가장 인기있는 부스!</p>
+
             <List/>
         </div>
     )
@@ -21,10 +37,12 @@ function List(){
                 <span className="place">B-1</span>
             </div>
             <div className="image">
-
+                <span className="name">예술대학 분장</span>
+                <span className="place">B-1</span>
             </div>
             <div className="image">
-
+                <span className="name">예술대학 분장</span>
+                <span className="place">B-1</span>
             </div>
         </div>
 
