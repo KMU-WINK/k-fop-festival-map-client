5bc4567feebac089c7410e0d2bd19596ca8b34e9
youngho-C <cyh6099@gmail.com>
2020-11-15 17:51:08 +0900

    Search page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import {ExampleComponent} from "./components/ExampleComponent";
 import {ReviewEdit} from "./components/ReviewEdit";
 import React from "react";
 
-import Search from "./components/Search/Search";
-
 function App() {
   return <>
-    <Search/>
   </>
 }
 
